01ddeba8dd2b5e4df6967d03a5a3bd2aee78fdf2
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-11-01 14:41:06 -0400

    Added info dynamiically to modal in html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,12 +11,28 @@ $(".quick-link-card").on("click", function () {
       },
       dataType: 'json',
   }).then(function (response) {
+    console.log(response);
+    $("#quickLinkScrollableModal .modal-body").empty();
       var business = response.businesses
       for ( var i = 0; i < business.length; i++){
       
       console.log(response);
       console.log(business[i].name);
-    
+    //  $("#quickLinkScrollableModal .modal-body").append(business[i].name);
+    //  $("#quickLinkScrollableModal .modal-body").append(business[i].location.address1);
+     var cardImage = $("<img>").attr("src", business[i].image_url);
+     cardImage.addClass("img-fluid");
+     var row = $("<div class='row mb-2'>")
+     var col4 = $("<div class='col-4'>");
+     col4.append(cardImage)
+     var col8 = $("<div class='col-8'>");
+     col8.append("<h4>" + business[i].name +"</h4>")
+     col8.append("<p>" + business[i].location.address1 +"</p>")
+
+
+     row.append(col4, col8);
+     console.log(cardImage);
+     $("#quickLinkScrollableModal .modal-body").append(row);
       }
 
   });
