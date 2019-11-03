a60a4cd42578dfedd8e68fdd6eba0fd34e8b1cf5
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-11-03 16:49:48 -0500

    creating bootstrap cards dynamically for search feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,51 @@
-$(".quick-link-card").on("click", function() {
+$(".yelpApiSearch").on("click", function (event) {
+  event.preventDefault();
+  console.log("Click!")
+
+  var search = $("#userInput").val();
+  console.log(search);
+
+  var queryURL =
+      "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" +
+      search +
+      "&location=philadelphia&radius=40000&limit=10&categories=kids,children";
+
+  $.ajax({
+      url: queryURL,
+      method: "GET",
+      headers: {
+          Authorization:
+              "Bearer xbvcW29zavUCvrlYQUoiGodM98RsayeaJSphvNMVW8WOY8RfwMsGR7NHtsqubWZhb620AwbXXVvGZG3R-Kui783rGEtf72eWS3BuTAqTQM0DsvaT-V6ddjw8sem6XXYx"
+      },
+      dataType: "json"
+  }).then(function (response) {
+      console.log(response);
+      $("#scrollDiv").empty();
+      var business = response.businesses;
+      for (var i = 0; i < business.length; i++) {
+          console.log(response);
+          console.log(business[i].name);
+          var cardImage = $("<img>").attr("src", business[i].image_url);
+          cardImage.addClass("img-fluid card-img-top");
+          var newCard = $("<div class='card'>");
+          var newCardBody = $("<div class='card-body'>");
+          newCardBody.append(cardImage);
+          newCardBody.append("<p>" + "<strong>" + business[i].name + "</strong>" + "</p>", "<p>" + business[i].location.address1 + "</p>", "<p>" + business[i].location.city + "</p>");
+
+          newCard.append(newCardBody);
+
+
+
+          $("#scrollDiv").append(newCard);
+
+
+
+      }
+  });
+})
+
+$(".quick-link-card").on("click", function(event) {
+  event.preventDefault();
   var term = $(this).attr("data-term");
   console.log(term);
   var queryURL =
@@ -104,32 +151,3 @@ function displayEvent() {
   });
 }
 displayEvent();
-
-// $(".quick-link-card").on("click", function () {
-//   var term = $(this).attr("data-term")
-//   console.log(term);
-//   var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + term + "&location=philadelphia&radius=40000&limit=10&categories=kids,children";
-
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET",
-//     headers: {
-//       Authorization: "Bearer xbvcW29zavUCvrlYQUoiGodM98RsayeaJSphvNMVW8WOY8RfwMsGR7NHtsqubWZhb620AwbXXVvGZG3R-Kui783rGEtf72eWS3BuTAqTQM0DsvaT-V6ddjw8sem6XXYx",
-//     },
-//     dataType: 'json',
-//   }).then(function (response) {
-//     console.log(response);
-//     $("#quickLinkScrollableModal .modal-body").empty();
-//     var business = response.businesses
-//     for (var i = 0; i < business.length; i++) {
-
-//       console.log(response);
-//       console.log(business[i].name);
-
-//       var searchDiv = $("div");
-//       searchDiv.append
-
-//     }
-
-//   });
-// });
